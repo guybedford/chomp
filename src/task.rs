1dfa998a09be75004cecf42a95b8f3ec527d7672
Guy Bedford <guybedford@gmail.com>
2022-02-24 16:23:39 -0800

    fix linux build again

--- src/task.rs
+++ src/task.rs
@@ -436,7 +436,7 @@ fn create_task_env<'a> (
             );
         }
     }
-    if let Some(task_env) = task.env {
+    if let Some(ref task_env) = task.env {
         for (item, value) in task_env {
             env.insert(
                 item.to_uppercase(),
@@ -448,7 +448,7 @@ fn create_task_env<'a> (
             );
         }
     }
-    if let Some(task_env_default) = task.env_default {
+    if let Some(ref task_env_default) = task.env_default {
         for (item, value) in task_env_default {
             if !env.contains_key(item) && std::env::var_os(item).is_none() {
                 env.insert(
