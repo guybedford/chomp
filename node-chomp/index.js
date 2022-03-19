83126ccf6cc76096b765482bf9999bc727a3bcea
Guy Bedford <guybedford@gmail.com>
2022-03-19 13:50:23 +0200

    fix: node-chomp vendor pathing

--- node-chomp/index.js
+++ node-chomp/index.js
@@ -15,7 +15,7 @@ const bin = new BinWrapper({ skipCheck: true })
   .src(`${base}/chomp-macos-${version}.tar.gz`, 'darwin')
   .src(`${base}/chomp-linux-${version}.tar.gz`, 'linux', 'x64')
   .src(`${base}/chomp-windows-${version}.zip`, 'win32', 'x64')
-  .dest(path.join('vendor'))
+  .dest(fileURLToPath(new URL('./vendor', import.meta.url)))
   .use(process.platform === 'win32' ? 'chomp.exe' : 'chomp')
   .version(version);
 
