ba497569a81360f9d36530b86907fc8dabe1ed76
Guy Bedford <guybedford@gmail.com>
2023-02-06 07:13:10 -0800

    0.2.12

--- src/main.rs
+++ src/main.rs
@@ -69,9 +69,9 @@ fn uri_parse(uri_str: &str) -> Option<Uri> {
 #[tokio::main]
 async fn main() -> Result<()> {
     #[cfg(not(debug_assertions))]
-    let version = "0.2.11";
+    let version = "0.2.12";
     #[cfg(debug_assertions)]
-    let version = "0.2.11-debug";
+    let version = "0.2.12-debug";
     let matches = Command::new("Chomp")
         .version(version)
         .arg(
